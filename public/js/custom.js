2348781997b9a974e6fcc6bf0e2627feb4da9f1f
deequoique <2751584869@qq.com>
2024-06-21 10:34:29 +0800

    1

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,6 +1,7 @@
 /* 轮播背景图片 */
 $(function () {
 	$.backstretch([
-		  "/image/background/back.jpg"
+		  "/image/background/back.jpg",
+		  "/image/background/th.jpg"
 	], { duration: 60000, fade: 1500 });
 });
